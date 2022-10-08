fc339328ac03ae30a3f9dbecac3b88de45a8f898
cavaldesm <cavaldesm@udd.cl>
2022-10-08 19:44:46 -0300

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import './App.css';
+import React from 'react';
 
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import Cart from './components/Cart';
+import CartProvider from './Context/CartContext';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export const CartContext = React.createContext([]);
+console.log('CartContext: ', CartContext);
 
 function App() {
   return (
     <>
     <BrowserRouter>
-    <NavBar />
-    <Routes>
+    <CartProvider>
+      <NavBar />
+      <Routes>
       <Route path='/' element={<ItemListContainer />} />
       <Route path='/categoria/:categoriaId' element={<ItemListContainer />} />
       <Route path='/cart' element={<Cart />} />
       <Route path='/detalle/:detalleId' element={<ItemDetailContainer />} />
-    </Routes>
+        </Routes>
+        </CartProvider>
     </BrowserRouter>
     </>
   );
---
+++ src/Context/CartContext.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const CartContext = React.createContext([]);
+
+const CartProvider = ({children}) => {
+
+    
+
+
+
+
+
+
+    return (
+        <CartContext.Provider>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider;
